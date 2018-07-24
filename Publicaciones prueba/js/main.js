2a52e5e18c9937dd0181a7eec9c4251f25e98b77
[Copelia] <iamcorallium@gmail.com>
2018-07-24 15:53:44 -0500

    Haciendo cambios a navbar en newsFeed para ajustarla a responsive. Modificando estilo de newAccount

--- Publicaciones prueba/js/main.js
+++
@@ -1,8 +0,0 @@
-// Firebase
-// Conexiones
- var db = firebase.database();
- var storage  = firebase.storage();
-//
-// // Referencias ...Rama?
- var housingRef = db.ref('housing/');
- var storageRef = storage.ref();
