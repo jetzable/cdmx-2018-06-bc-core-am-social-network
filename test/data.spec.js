4a86c1e0c744ea61e5dce12cd0c57b19463e45cf
[Copelia] <iamcorallium@gmail.com>
2018-07-27 16:05:06 -0500

    Añadiendo prueba de test a una función

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,1 +1,24 @@
 // Ejecuta tu test 
+
+describe('loginUser', () => {
+  it('es una funcion', () => {
+    assert.isFunction(loginUser);
+  it('debería permitir ingresar a usuario', () => {
+    //assert.algoaquí(loginUser);
+  })  
+  });
+});
+
+describe('Debería permitir publicar', () => {
+  it('es una funcion', () => {
+    assert.isFunction(addingDataToNewsfeed);
+  });
+});
+
+describe('Post debería ser del tipo string', () => {
+  it('es del tipo string', ()=>{
+    assert.equal(typeof addingDataToNewsfeed, 'string');
+  });
+});
+
+
